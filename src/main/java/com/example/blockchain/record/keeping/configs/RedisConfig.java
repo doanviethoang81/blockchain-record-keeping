7b2f42aa896cc193cc6b88e2fa97f0e7192c3f7a
Doan Viet Hoang <hoangdoanviet81@gmail.com>
2025-05-10 00:55:53 +0700

    upload url FE 4

--- src/main/java/com/example/blockchain/record/keeping/configs/RedisConfig.java
+++ src/main/java/com/example/blockchain/record/keeping/configs/RedisConfig.java
@@ -8,25 +8,27 @@ import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.repository.configuration.EnableRedisRepositories;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
+@EnableRedisRepositories
 public class RedisConfig {
 
-    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
-    String host = dotenv.get("REDIS_HOST");
-    String password = dotenv.get("REDIS_PASSWORD");
-//
-//    @Value("${spring.data.redis.host}")
-//    private String host;
+//    Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
+//    String host = dotenv.get("REDIS_HOST");
+//    String password = dotenv.get("REDIS_PASSWORD");
+
+    @Value("${spring.data.redis.host}")
+    private String host;
 
     @Value("${spring.data.redis.port}")
     private int port;
 
-//    @Value("${spring.data.redis.password}")
-//    private String password;
+    @Value("${spring.data.redis.password:}")
+    private String password;
 
-    @Value("${spring.data.redis.ssl.enabled}")
+    @Value("${spring.data.redis.ssl.enabled:false}")
     private boolean sslEnabled;
 
     @Bean
